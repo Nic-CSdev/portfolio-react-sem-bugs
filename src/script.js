221fe8e00e067d50cbe7a7c5fdb6162761c699d7
Nicolas Coelho <nicolascoelhosp@hotmail.com>
2023-04-20 20:52:49 -0300

    voltando para como estava antes, não funcionou

--- src/script.js
+++ src/script.js
@@ -1,13 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
 window.addEventListener('load', () => {
-  const dropdown = document.querySelector('.dropdown');
-  if (dropdown) {
-    dropdown.addEventListener('touchstart', () => {
-      dropdown.classList.toggle('active');
-    });
-    dropdown.addEventListener('touchend', () => {
-      dropdown.classList.toggle('active');
-    });
-  }
-})
-});
+    const dropdown = document.querySelector('.dropdown');
+    if (dropdown) {
+      dropdown.addEventListener('click', () => {
+        dropdown.classList.toggle('active');
+      });
+    }
+  });
+  
