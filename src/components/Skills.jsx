9002cac06de1a6b9e1f8a75f35e2ffce755b5b77
Nicolas Coelho <nicolascoelhosp@hotmail.com>
2023-05-25 11:49:31 -0300

    voltando a parte de skills a como era antes e adicionando efeito de flip-box nela

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,121 +1,89 @@
 import React from 'react'
-import './experience.css'
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JS from '../assets/javascript.png';
+import Node from '../assets/node.png';
+import REACT from '../assets/react.png';
+import Tailwind from '../assets/Tailwind.png';
 const Skills = () => {
-  return (
-    <section name='skills' id='skills' className='experience-section bg-[#030507] text-gray-300 skills-content' >
-        <div className=' container max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full pt-28'>
-            <div className='section_title text-center'>
-                
-                <p className='text-gray-300 border-purple-700 text-4xl font-bold inline border-b-4'>Habilidades</p>
-                <h2 className='text-purple-800'>Minha área de experiência</h2>
+    return (
+        <div name='skills' id='skills' className='bg-[#030507] text-gray-300 skills-content' >
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full pt-28'>
+                <div>
+                    <p className='text-4xl font-bold inline border-b-4 border-purple-700'>Habilidades</p>
+                    <p className='py-4'>// Algumas tecnologias com as quais eu trabalhei</p>
+                </div>
+                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                    <div className='card'>
+                        <div className="card-inner">
+                            <div className="card-front">
+                                <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+                                <p className='my-4'>HTML</p>
+                            </div>
+                            <div className="card-back">
+                                <p>Experiência em HTML5 desde os conceitos básicos até alguns mais avançados</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className="card-inner">
+                            <div className="card-front">
+                                <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+                                <p className='my-4'>CSS</p>
+                            </div>
+                            <div className="card-back">
+                                <p>Experiência em CSS3 desde os conceitos básicos até alguns mais avançados</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className="card-inner">
+                            <div className="card-front">
+                                <img className='w-20 mx-auto' src={JS} alt="JavaScript icon" />
+                                <p className='my-4'>JS</p>
+                            </div>
+                            <div className="card-back">
+                                <p>Experiência baseada apenas em conceitos básicos sobre Javascript</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className="card-inner">
+                            <div className="card-front">
+                                <img className='w-20 mx-auto' src={REACT} alt="React icon" />
+                                <p className='my-4'>React</p>
+                            </div>
+                            <div className="card-back">
+                                <p>Experiência baseada no conhecimento sobre conceitos desde básicos à intermediários sobre ReactJS</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card' >
+                        <div className="card-inner">
+                            <div className="card-front">
+                                <img className='w-20 mx-auto' src={Node} alt="Node.js icon" />
+                                <p className='my-4'>Node</p>
+                            </div>
+                            <div className="card-back">
+                                <p>Experiência baseada apenas em conceitos básicos sobre NodeJS</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card' >
+                        <div className="card-inner">
+                            <div className="card-front">
+                                <img className='w-20 mx-auto' src={Tailwind} alt="Node.js icon" />
+                                <p className='my-4'>Tailwind</p>
+                            </div>
+                            <div className="card-back">
+                                <p>Experiência baseada no conhecimento sobre conceitos desde básicos à intermediários sobre o framework Tailwind</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-            <div className='experience-items'>
-
-                <div className='experience-item'>
-                    <div className="experience-info">
-                        <p>HTML</p>
-                        <p>90%</p>
-                    </div>
-                    <div className='progress-line' data-percent='90%'>
-                        <span style={{width: '90%'}}></span>
-                    </div>
-                </div>
-
-                <div className='experience-item'>
-                    <div className="experience-info">
-                        <p>CSS</p>
-                        <p>90%</p>
-                    </div>
-                    <div className='progress-line' data-percent='90%'>
-                        <span style={{width: '90%'}}></span>
-                    </div>
-                </div>
-
-                <div className='experience-item'>
-                    <div className="experience-info">
-                        <p>JS</p>
-                        <p>60%</p>
-                    </div>
-                    <div className='progress-line' data-percent='60%'>
-                        <span style={{width: '60%'}}></span>
-                    </div>
-                </div>
-
-                <div className='experience-item'>
-                    <div className="experience-info">
-                        <p>SQL</p>
-                        <p>50%</p>
-                    </div>
-                    <div className='progress-line' data-percent='50%'>
-                        <span style={{width: '50%'}}></span>
-                    </div>
-                </div>
-
-                <div className='experience-item'>
-                    <div className="experience-info">
-                        <p>React</p>
-                        <p>40%</p>
-                    </div>
-                    <div className='progress-line' data-percent='40%'>
-                        <span style={{width: '40%'}}></span>
-                    </div>
-                </div>
-
-                <div className='experience-item'>
-                    <div className="experience-info">
-                        <p>Node</p>
-                        <p>20%</p>
-                    </div>
-                    <div className='progress-line' data-percent='20%'>
-                        <span style={{width: '20%'}}></span>
-                    </div>
-                </div>
-
-                <div className='experience-item'>
-                    <div className="experience-info">
-                        <p>C#</p>
-                        <p>30%</p>
-                    </div>
-                    <div className='progress-line' data-percent='30%'>
-                        <span style={{width: '30%'}}></span>
-                    </div>
-                </div>
-
-                <div className='experience-item'>
-                    <div className="experience-info">
-                        <p>Java</p>
-                        <p>30%</p>
-                    </div>
-                    <div className='progress-line' data-percent='30%'>
-                        <span style={{width: '30%'}}></span>
-                    </div>
-                </div>
-
-                <div className='experience-item'>
-                    <div className="experience-info">
-                        <p>PHP</p>
-                        <p>40%</p>
-                    </div>
-                    <div className='progress-line' data-percent='40%'>
-                        <span style={{width: '40%'}}></span>
-                    </div>
-                </div>
-
-                <div className='experience-item'>
-                    <div className="experience-info">
-                        <p>Python</p>
-                        <p>30%</p>
-                    </div>
-                    <div className='progress-line' data-percent='30%'>
-                        <span style={{width: '30%'}}></span>
-                    </div>
-                </div>
-
-            </div>
-
         </div>
-    </section>
-  )
+    )
 }
-
 export default Skills
